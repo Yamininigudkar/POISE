3a37e49f30d001be33d079847387b66598c32a54
Yamini <yaminipuranik@gmail.com>
2017-11-30 11:40:11 -0600

    api key

--- client/src/pages/Notch/Map.js
+++ client/src/pages/Notch/Map.js
@@ -15,7 +15,7 @@ const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https:/"+"/maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https:/"+"/maps.googleapis.com/maps/api/js?key=AIzaSyDM80HdqN8I7OZaOY9B8MUjFa3kguMhB_E.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
